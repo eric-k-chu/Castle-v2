4c15b1ba2812c197e548898b7ebe4406283504cc
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-23 01:02:01 -0800

    renamed chess api url

--- castle/app/chessapi/player/get-player-data.ts
+++ castle/app/chessapi/player/get-player-data.ts
@@ -1,8 +1,8 @@
-import { CHESS_API_PREFIX } from "@/lib/constants";
+import { CHESS_API_URL } from "@/lib/constants";
 import { getError } from "@/utils";
 
 export async function getPlayerData<T>(slug: string): Promise<T> {
-  const res = await fetch(CHESS_API_PREFIX + slug);
+  const res = await fetch(CHESS_API_URL + slug);
   if (!res.ok) throw new Error(getError(res.status));
   return await res.json();
 }
