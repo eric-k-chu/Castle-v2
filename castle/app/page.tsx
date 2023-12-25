ea5e39806196c41b3fb780064ece0979fe82b98f
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-24 22:34:34 -0800

    WIP refactor error handling

--- castle/app/page.tsx
+++ castle/app/page.tsx
@@ -1,29 +1,22 @@
-"use client";
-
-import { ErrorDisplay, Loading, SearchInput } from "./components";
-import { useFetcher } from "./hooks/useFetcher";
+import { SearchInput } from "./components";
 import { getPlayerSuggestions } from "./utils/fetcher";
 
-export default function Home() {
-  const { data, isLoading, error } = useFetcher(getPlayerSuggestions);
+export default async function Home() {
+  try {
+    const players = await getPlayerSuggestions();
 
-  if (isLoading) return <Loading />;
-
-  if (error) return <ErrorDisplay error={error as Error} />;
-
-  if (!data) return <Loading />;
-
-  return (
-    <main className="flex min-h-screen flex-col items-center bg-hero bg-cover bg-top bg-no-repeat">
-      <div className="mb-10 mt-64 text-center">
-        <h1 className="text-2xl font-semibold uppercase drop-shadow-glow sm:text-4xl">
-          Castle
-        </h1>
-        <p className="mt-4 text-base font-medium text-gray-200 drop-shadow-glow sm:text-xl">
-          A player search engine. Powered by Chess.com API
-        </p>
-      </div>
-      <SearchInput players={data} />
-    </main>
-  );
+    return (
+      <main className="flex min-h-screen flex-col items-center bg-hero bg-cover bg-top bg-no-repeat">
+        <div className="mb-10 mt-64 text-center">
+          <h1 className="text-2xl font-semibold uppercase drop-shadow-glow sm:text-4xl">
+            Castle
+          </h1>
+          <p className="mt-4 text-base font-medium text-gray-200 drop-shadow-glow sm:text-xl">
+            A player search engine. Powered by Chess.com API
+          </p>
+        </div>
+        <SearchInput players={players} />
+      </main>
+    );
+  } catch (err) {}
 }
