1b172af022c5c20a611b8bdf8cfc762b1a4b62f0
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-26 14:29:54 -0800

    removed unused folder

--- castle/app/chessapi/clubs/get-club.ts
+++
@@ -1,6 +0,0 @@
-import { Club } from "@/lib/types";
-
-// export async function getClub(urlId: string): Promise<ChessAPIData<Club>> {
-//   const id = urlId.trim().toLocaleLowerCase().split(" ").join("-");
-//   const res = await fetch(`https://api.chess.com/pub/club/${id}`);
-// }
