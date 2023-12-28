bbd60314a8415d681539750b79002a9de803db03
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-27 23:50:19 -0800

    completed player-clubs page

--- castle/app/(routes)/search/_components/ClubsDisplay.tsx
+++ castle/app/(routes)/search/_components/ClubsDisplay.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import Image from "next/image";
 import { ErrorDisplay, LoadingCircle } from "@/_components";
 import { useFetcher } from "@/_hooks/useFetcher";
+import { getDateFromUtc } from "@/_utils";
 import { getPlayerClubs } from "@/_utils/fetcher";
 
 type Props = {
@@ -28,12 +32,53 @@ export function ClubsDisplay({ username }: Props) {
   if (!clubs) return null;
 
   return (
-    <div className="flex w-full flex-col items-center p-4">
-      <ul>
-        {clubs.clubs.map((n) => (
-          <li key={n["@id"]}>{n.name}</li>
-        ))}
-      </ul>
+    <div className="flex w-full flex-col items-center">
+      <table className="w-full text-xs sm:text-sm">
+        <thead>
+          <tr className="uppercase text-gray-200">
+            <th>&nbsp;</th>
+            <th className="text-left">club</th>
+            <th className="hidden px-6 text-left sm:table-cell">joined</th>
+            <th className="hidden px-6 text-left sm:table-cell">
+              last activity
+            </th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody className="uppercase">
+          {clubs.clubs
+            .sort((a, b) => b.joined - a.joined)
+            .map((n) => (
+              <tr key={n["@id"]} className="border border-gray-600 bg-black">
+                <td className="p-4">
+                  <img
+                    src={n.icon}
+                    alt={`${n.name} club icon`}
+                    className="h-6 min-h-[1.5rem] w-6 min-w-[1.5rem] rounded-full"
+                  />
+                </td>
+                <td className="py-4">{n.name}</td>
+                <td className="hidden px-6 py-4 sm:table-cell">
+                  {getDateFromUtc(n.joined)}
+                </td>
+                <td className="hidden px-6 py-4 sm:table-cell">
+                  {getDateFromUtc(n.last_activity)}
+                </td>
+                <td className="py-4 pl-6 text-right">
+                  <a href={n.url} target="_blank">
+                    <Image
+                      src="/icons/dots.svg"
+                      alt="link"
+                      width="0"
+                      height="0"
+                      className="h-auto w-4"
+                    />
+                  </a>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
