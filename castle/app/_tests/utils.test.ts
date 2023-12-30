c529a61b508e9dd28da160cc0be697fbe2f6445d
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-30 15:59:45 -0800

    Added new tests

--- castle/app/_tests/utils.test.ts
+++ castle/app/_tests/utils.test.ts
@@ -1,4 +1,4 @@
-import { getGameResultColor } from "@/_utils";
+import { getDateFromUtc, getGameResultColor } from "@/_utils";
 
 describe("getGameResultColor", () => {
   it("win", () => {
@@ -29,5 +29,44 @@ describe("getGameResultColor", () => {
     const pgn = `[Event "Live Chess"]\n[Site "Chess.com"]\n[Date "2023.12.02"]\n[Round "-"]\n[White "erik"]\n[Black "Lubbipapi"]\n[Resul "1-0"]\n`;
     const result = getGameResultColor("erik", pgn);
     expect(result).toBe("border-l-black");
+  });
+});
+
+describe("getDateFromUtc", () => {
+  it("January", () => {
+    expect(getDateFromUtc(1673854677)).toEqual("Jan 15, 2023");
+  });
+  it("February", () => {
+    expect(getDateFromUtc(1676533077)).toEqual("Feb 15, 2023");
+  });
+  it("March", () => {
+    expect(getDateFromUtc(1678948677)).toEqual("Mar 15, 2023");
+  });
+  it("April", () => {
+    expect(getDateFromUtc(1681627077)).toEqual("Apr 15, 2023");
+  });
+  it("May", () => {
+    expect(getDateFromUtc(1684219077)).toEqual("May 15, 2023");
+  });
+  it("June", () => {
+    expect(getDateFromUtc(1686897477)).toEqual("Jun 15, 2023");
+  });
+  it("July", () => {
+    expect(getDateFromUtc(1689489477)).toEqual("Jul 15, 2023");
+  });
+  it("August", () => {
+    expect(getDateFromUtc(1692167877)).toEqual("Aug 15, 2023");
+  });
+  it("September", () => {
+    expect(getDateFromUtc(1694846277)).toEqual("Sep 15, 2023");
+  });
+  it("Ocotober", () => {
+    expect(getDateFromUtc(1697438277)).toEqual("Oct 15, 2023");
+  });
+  it("November", () => {
+    expect(getDateFromUtc(1700120277)).toEqual("Nov 15, 2023");
+  });
+  it("December", () => {
+    expect(getDateFromUtc(1702712277)).toEqual("Dec 15, 2023");
   });
 });
