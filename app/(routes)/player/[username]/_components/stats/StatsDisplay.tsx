4642e02e03e87b6da0ffcc51271268abcaf7f27f
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-31 20:49:28 -0800

    added tooltip

--- app/(routes)/player/[username]/_components/stats/StatsDisplay.tsx
+++ app/(routes)/player/[username]/_components/stats/StatsDisplay.tsx
@@ -17,7 +17,7 @@ export function StatsDisplay({ stats }: Props) {
     <section className="rounded-sm bg-zinc-900 p-6">
       <div className="flex flex-1 flex-wrap items-center justify-around gap-8">
         {gameStats.map((n) => (
-          <div className="space-y-2" key={n.type}>
+          <div className="group relative space-y-2" key={n.type}>
             <strong className="text-sm uppercase sm:text-base">{n.type}</strong>
             <h2
               className={`text-base sm:text-lg ${
@@ -30,6 +30,23 @@ export function StatsDisplay({ stats }: Props) {
             <h3 className="text-xs text-zinc-400">
               {n.wins + n.losses + n.draws} games
             </h3>
+            <div
+              role="tooltip"
+              className="absolute bottom-0 left-10 z-10 hidden space-y-2 rounded-md bg-zinc-700 p-4 text-xs group-hover:block"
+            >
+              <div className="flex text-green-400">
+                <span className="pr-1">wins:</span>
+                <span>{n.wins}</span>
+              </div>
+              <div className="flex text-red-400">
+                <span className="pr-1">losses:</span>
+                <span>{n.losses}</span>
+              </div>
+              <div className="flex text-zinc-400">
+                <span className="pr-1">draws:</span>
+                <span>{n.draws}</span>
+              </div>
+            </div>
           </div>
         ))}
         <Show when={tactics !== undefined}>
