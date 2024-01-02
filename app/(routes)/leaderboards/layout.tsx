4136e544010065ca242a3b638ee3623bf98c7a94
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-02 14:33:27 -0800

    added initial leaderboards and streamers pages

---
+++ app/(routes)/leaderboards/layout.tsx
@@ -0,0 +1,9 @@
+import { PageLayout } from "@/_components";
+
+export default function LeaderboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <PageLayout>{children}</PageLayout>;
+}
