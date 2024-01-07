82766ad1f753f064562baa836889d48e0f90914d
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-07 01:12:49 -0800

    resolved import errors

--- app/_chessapi/streamer/get-streamers.ts
+++ app/_chessapi/streamer/get-streamers.ts
@@ -1,6 +1,6 @@
 import { CHESS_API_BASE } from "@/_lib/constants";
 import { Streamers } from "@/_lib/types";
-import { ChessApiError } from "@/_utils/exceptions";
+import { ChessApiError } from "@/_utils";
 
 export async function getStreamers(): Promise<Streamers> {
   const res = await fetch(CHESS_API_BASE + "streamers");
