5de515048d401b239f0428e0f31a1481b057f36a
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-07 01:21:41 -0800

    added streamers

--- app/(routes)/streamers/page.tsx
+++ app/(routes)/streamers/page.tsx
@@ -1,4 +1,8 @@
-export default function Streamers() {
+import { getStreamers } from "@/_chessapi/streamer";
+
+export default async function Streamers() {
+  const streamers = await getStreamers();
+
   return (
     <div className="mx-auto w-full max-w-5xl px-4 pb-[100px] pt-40">
       <h1 className="text-center">Streamers</h1>
