b74850671f59fb31272de526f518f57084781061
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-09 01:54:48 -0800

    added filter to countries

--- app/(routes)/countries/page.tsx
+++ app/(routes)/countries/page.tsx
@@ -1,15 +1,41 @@
+"use client";
+
 /* eslint-disable @next/next/no-img-element */
+import { SearchIcon, WorldIcon } from "@/_components";
 import { Country } from "@/_lib";
 import { getCountries } from "@/_utils";
+import { useMemo, useState } from "react";
 
 export default function Countries() {
-  const countries = getCountries();
+  const [filter, setFilter] = useState("");
+  const countries = useMemo(() => getCountries(), []);
+
+  const filteredCountries = countries.filter(
+    (n) =>
+      n.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()) ||
+      n.code.toLocaleLowerCase().includes(filter.toLocaleLowerCase()),
+  );
 
   return (
     <div className="mx-auto w-full px-4 py-24 sm:max-w-lg min-[878px]:max-w-3xl lg:max-w-4xl">
-      <h1 className="text-center">Countries</h1>
+      <div className="mb-2 flex items-center">
+        <div className="flex items-center gap-x-2 sm:gap-x-4">
+          <WorldIcon className="inline h-auto w-5 fill-zinc-200 sm:w-6 md:w-8" />
+          <strong className="text-lg uppercase sm:text-2xl md:text-4xl">
+            Countries
+          </strong>
+        </div>
+        <section className="ml-4 flex items-center border-b border-b-zinc-200 p-1 focus-within:ring-1 focus-within:ring-blue-600">
+          <input
+            className="bg-transparent focus:outline-none"
+            onChange={(e) => setFilter(e.currentTarget.value)}
+            value={filter}
+          />
+          <SearchIcon className="h-auto w-4 fill-zinc-200" />
+        </section>
+      </div>
       <div className="flex flex-wrap">
-        {countries.map((n, i) => (
+        {filteredCountries.map((n, i) => (
           <div
             key={i}
             className="w-full p-1 sm:w-1/2 min-[878px]:w-1/3 lg:w-1/4"
