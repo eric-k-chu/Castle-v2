3adcd35f63e11e1bb764bc7973eccff81a7fef20
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-09 20:58:22 -0800

    Added small footer

--- app/_components/ChessImgBackground.tsx
+++ app/_components/ChessImgBackground.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ReactNode } from "react";
 
 type Props = {
@@ -7,13 +8,29 @@ type Props = {
 export function ChessImgBackground({ children }: Props) {
   return (
     <main
-      className="bg-zinc-800"
+      className="relative"
       style={{
         background:
           "conic-gradient(from 90deg at 0.5px 0.5px,#18181b 90deg,#27272a 0) 0 0/50px 50px",
       }}
     >
       {children}
+      <div className="flex h-72 items-end justify-center bg-gradient-to-t from-zinc-900 from-70% to-transparent pb-10">
+        <a
+          target="_blank"
+          href="https://github.com/eric-k-chu"
+          className="flex items-center"
+        >
+          <Image
+            src="/icons/github.svg"
+            alt="github link"
+            width={0}
+            height={0}
+            className="h-auto w-8"
+          />
+          <span className="px-2 text-lg">GitHub</span>
+        </a>
+      </div>
     </main>
   );
 }
