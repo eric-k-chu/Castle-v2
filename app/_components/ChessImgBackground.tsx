65e4c4b9fc985b0ef2a27d7b1008060ffcb38142
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-14 18:27:38 -0800

    removed footer

--- app/_components/ChessImgBackground.tsx
+++ app/_components/ChessImgBackground.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import { ReactNode } from "react";
-import { GitHubIcon } from ".";
 
 type Props = {
   children: ReactNode;
@@ -9,22 +7,10 @@ type Props = {
 export function ChessImgBackground({ children }: Props) {
   return (
     <main
-      className="bg-gradient-conic-light dark:bg-gradient-conic-dark relative"
+      className="relative bg-gradient-conic-light dark:bg-gradient-conic-dark"
       style={{ backgroundPosition: "0 0", backgroundSize: "50px 50px" }}
     >
       {children}
-      <div className="flex h-72 items-end justify-center bg-gradient-to-t from-neutral-300 from-70% to-transparent pb-10 dark:from-neutral-950">
-        <a
-          target="_blank"
-          href="https://github.com/eric-k-chu"
-          className="flex items-center"
-        >
-          <GitHubIcon />
-          <span className="px-2 text-lg text-neutral-900 dark:text-neutral-200">
-            GitHub
-          </span>
-        </a>
-      </div>
     </main>
   );
 }
