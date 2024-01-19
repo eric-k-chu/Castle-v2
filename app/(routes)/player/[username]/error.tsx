12d4030e46a23e12e9984419a4759e1491a4b15a
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-19 01:50:36 -0800

    logging error in error component

--- app/(routes)/player/[username]/error.tsx
+++ app/(routes)/player/[username]/error.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { ErrorMessage } from "@/_components";
+import { useEffect } from "react";
 
 type Props = {
-  error: Error;
+  error: Error & { digest?: string };
 };
 
 export default function Error({ error }: Props) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
   return (
     <ErrorMessage
       message={error instanceof Error ? error.message : "Unexpected Error."}
